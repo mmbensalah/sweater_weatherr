cd464a24cce75b0901088c275ee11c3ad3282718
mmbensalah <mmbensalah@gmail.com>
2019-01-04 09:29:05 -0700

    creates request test for giphy

---
+++ spec/requests/api/v1/giphy_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'GET /api/v1/gifs?location=denver,co' do
+  it 'returns time, summary, gif url' do
+    location = 'Denver, CO'
+
+    get "/api/v1/gifs?location=#{location}"
+
+    expect(response).to be_successful
+    parsed_response = JSON.parse(response.body)
+
+    expect(parsed_response).to have_key(:data)
+    expect(parsed_response[:data][:daily_forecasts][0][url]).to eq("/") 
+
+  end
+end
