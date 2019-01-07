aaf704eaaae3a9205a50de9164ebfca7cbbbc7a4
mmbensalah <mmbensalah@gmail.com>
2019-01-06 21:59:17 -0700

    add status 401 in case POST to api/v1/favorite does not have api key

--- spec/requests/api/v1/favorite_request_spec.rb
+++ spec/requests/api/v1/favorite_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'POST /api/v1/favorites' do
-  it 'creates a favorite location for user' do
+  it 'creates a favorite location object for user' do
     user = User.create(email: 'email@gmail.com', password: '12345', password_confirmation: '12345', api_key: 'abc123')
     location = 'Denver, CO'
 
@@ -12,5 +12,17 @@ describe 'POST /api/v1/favorites' do
     post "/api/v1/favorites", params: data
 
     expect(Favorite.last.user_id).to eq(user.id)
+  end
+  it 'does not create a favorite location for user without api key' do
+    user = User.create(email: 'email@gmail.com', password: '12345', password_confirmation: '12345', api_key: 'abc123')
+    location = 'Denver, CO'
+
+    data = {location: location
+           }
+
+    post "/api/v1/favorites", params: data
+
+    expect(response.status).to eq(401)
+    expect(Favorite.last).to eq(nil)
   end
 end
