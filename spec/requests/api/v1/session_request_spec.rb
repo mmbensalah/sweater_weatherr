cd4982a36bad57a36aab7b8f75baa93b7a11e309
mmbensalah <mmbensalah@gmail.com>
2019-01-06 19:14:13 -0700

    move session request test from user to own file

---
+++ spec/requests/api/v1/session_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'POST /api/v1/sessions' do
+  it 'creates new user session for created user' do
+
+    user = User.create(email: 'email@gmail.com', password: '12345', password_confirmation: '12345', api_key: 'abc123')
+    # data = {email: 'email@gmail.com',
+    #         password: '12345'
+    #         }
+
+    post "/api/v1/sessions?email=email@gmail.com&password=12345"
+
+    expect(response.status).to eq(200)
+    parsed_response = JSON.parse(response.body)
+    key = parsed_response["data"]["attributes"]["api_key"]
+    expect(key).to eq(User.last.api_key)
+  end
+end
