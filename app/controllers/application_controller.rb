ce08301f5f56c49655bd8435e06d9e12b382af7b
mmbensalah <mmbensalah@gmail.com>
2019-01-08 20:49:39 -0700

    add invalid_credentials method to app controller to call from fav controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def invalid_credentials
+    render json: { message: 'Invalid Credentials' }, status: 401
+  end
 end
